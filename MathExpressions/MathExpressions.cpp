ab7b8df0228f0c33df555ea0cc6bacabb0bf1985
Adrian <107988407+rjaks@users.noreply.github.com>
2024-09-30 15:51:58 +0800

    Issue: ubos na braincells q

--- MathExpressions/MathExpressions.cpp
+++ MathExpressions/MathExpressions.cpp
@@ -1,28 +1,34 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 const string numbers = "0123456789";
 const string operators = "+-*/^";
 const string parantheses = "()";
 bool isNumber, isOperator, isParanthesis;
+vector<char> op;
+string output = "";
 
-class Node {
-    public:
-        char value;
-        Node* next;
-};
-
-void solve();
+void solve(string);
 void check(char);
 void push(char);
+void pop();
 void display();
+
+// 5 + 7 - (5 * 3) / 8
+// 5 7 + 5 3 * - 8 /
 
 void solve(string C){
     for (int i = 0; i < C.length(); i++){
         isNumber = false, isOperator = false, isParanthesis = false;
         check(C[i]);
-        if (isParanthesis) check(C[i+1]);
-        else if (isNumber) push(C[i]);
+        
+        if (isNumber) output += C[i];
+        else if (isNumber && op.size() > 0) {
+            output += C[i];
+            pop();
+        }
+        else if (isOperator) op.push_back(C[i]);
     }
 }
 
@@ -38,6 +44,15 @@ void check(char c){
     }
 }
 
+void pop(){
+    output += op[0];
+    op.erase(op.begin());
+}
+
+void display(){
+    for (char k: output) cout << k << " ";
+}
+
 int main(){
     int E;
     cin >> E;
@@ -47,6 +62,7 @@ int main(){
         cin.ignore();
         getline(cin, C);
         solve(C);
+        display();
     }
 
     return 0;
