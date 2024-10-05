f1dc44bbd4d6c8b9885debf8df3df685289b7ca4
rjaks <adrianreforsado1@gmail.com>
2024-10-05 16:23:22 +0800

    In progress: Logical errors

---
+++ BankTeller/BankTeller.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> vip;
+vector<string> regular;
+
+void solve(){
+    int rank; string command, name;
+    cin >> command >> rank >> name;
+    if (command == "COME"){
+        if (rank == 1){
+            vip.insert(regular.begin(), name);
+        }
+        else if (rank == 2){
+            regular.insert(regular.begin(), name);
+        }
+    }
+    else if (command == "SERVE"){
+        if (vip.size() != 0){
+            cout << vip.back() << endl; vip.pop_back();
+        }
+        else {
+            if (regular.size() != 0){
+                cout << regular.back() << endl; regular.pop_back();
+            }
+            else cout << "EMPTY\n";
+        }
+    }
+}
+
+int main(){
+    int N;
+    cin >> N;
+
+    for (int i = 0 ; i < N; i++) { solve(); }
+
+    return 0;
+}
