37e5b30e8bcfeffaa2b68609c9e5a21e7448413e
rjaks <adrianreforsado1@gmail.com>
2024-10-05 20:40:57 +0800

    Solved with vectors: Revised

--- BankTeller/BankTeller.cpp
+++ BankTeller/BankTeller.cpp
@@ -11,19 +11,21 @@ void solve(){
     if (command == "COME"){
         cin >> rank >> name;
         if (rank == 1){
-            vip.insert(regular.begin(), name);
+            if (vip.size() == 0) vip.push_back(name);
+            else vip.insert(regular.begin(), name);
         }
         else if (rank == 2){
-            regular.insert(regular.begin(), name);
+            if (regular.size() == 0) regular.push_back(name);
+            else regular.insert(regular.begin(), name);
         }
     }
     else if (command == "SERVE"){
         if (vip.size() != 0){
-            cout << vip.back() << endl; vip.pop_back();
+            cout << vip.front() << endl; vip.erase(vip.begin());
         }
         else {
             if (regular.size() != 0){
-                cout << regular.back() << endl; regular.pop_back();
+                cout << regular.front() << endl; regular.erase(regular.begin());
             }
             else cout << "EMPTY\n";
         }
